94cfd253e1d9673a8e11d57fe8236e13e69d53c3
unusualpseudo <spik3spiegel@proton.me>
2023-07-12 01:42:05 +0200

    feat(github): merge nexus repository state
    Signed-off-by: unusualpseudo <spik3spiegel@proton.me>

--- github/main.tf
+++ github/main.tf
@@ -3,6 +3,7 @@ locals {
     "terraformars" : { "description" = "home to my iac experiments", "topics" = ["terraform"] }
     "provision" : { "description" = "home to my ansible experiments", "topics" = ["ansible"] }
     "dotfiles" : { "description" = "dotfiles for my ubuntu", "topics" = ["fish", "chezmoi"] }
+    "nexus" : { "description" = "my home lab", "topics" = ["k8s", "k3s", "homelab", "ansible", "fluxcd"] }
   }
 }
 
