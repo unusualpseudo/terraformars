3dc815dc7df8c3c9c50575a644e266f67b2fe068
unusualpseudo <spik3spiegel@proton.me>
2023-07-14 21:36:52 +0200

    fix(github): stop using tfvars and put data in default
    Signed-off-by: unusualpseudo <spik3spiegel@proton.me>

--- terraform/github/variables.tf
+++ terraform/github/variables.tf
@@ -3,4 +3,22 @@ variable "repositories" {
     description = string
     topics      = list(string)
   }))
+  default = {
+    "terraformars" = {
+      description = "home to my iac experiments"
+      topics      = ["terraform"]
+    }
+    "provision" = {
+      description = "home to my ansible experiments"
+      topics      = ["ansible"]
+    }
+    "dotfiles" = {
+      description = "dotfiles for my ubuntu"
+      topics      = ["fish", "chezmoi"]
+    }
+    "nexus" = {
+      description = "my home lab"
+      topics      = ["k8s", "k3s", "homelab", "ansible", "fluxcd"]
+    }
+  }
 }
